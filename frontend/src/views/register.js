378ebaba2c5df31d2f230690978b914da6d4e1e5
UlithG18 <ulith.software@gmail.com>
2026-03-13 15:26:01 -0500

    fix: endpoint port number

--- frontend/src/views/register.js
+++ frontend/src/views/register.js
@@ -120,7 +120,7 @@ export function registerEvents() {
         }
 
         try {
-            const response = await fetch("http://localhost:4000/api/register", {
+            const response = await fetch("http://localhost:3000/api/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
