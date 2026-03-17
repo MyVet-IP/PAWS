8541a950481de88515e696bf0f1ce5e512748fec
UlithG18 <ulith.software@gmail.com>
2026-03-17 16:46:46 -0500

    aja

--- frontend/src/views/medical-records.js
+++ frontend/src/views/medical-records.js
@@ -610,7 +610,7 @@ export function medicalRecordsEvents() {
         success: '<svg style="width:1em;height:1em;display:inline-block;vertical-align:middle;" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>',
         error: '<svg style="width:1em;height:1em;display:inline-block;vertical-align:middle;" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>'
       };
-      status.textContent = `${icons[type]} ${message}`;
+      status.innerHTML = `${icons[type]} ${message}`;
     }
   }
 
